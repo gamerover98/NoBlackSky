c2f74d77712e47569420adeb50a2fb62eda4d587
gamerover98 <gamerover98@hotmail.it>
2020-11-19 03:19:07 +0100

    Initialized custom packet editor for 1.16 server version and above

--- src/main/java/it/gamerover/nbs/packet/NoBlackSkyAdapter.java
+++ src/main/java/it/gamerover/nbs/packet/NoBlackSkyAdapter.java
@@ -2,7 +2,9 @@ package it.gamerover.nbs.packet;
 
 import com.comphenix.packetwrapper.WrapperPlayServerLogin;
 import com.comphenix.packetwrapper.WrapperPlayServerRespawn;
+import it.gamerover.nbs.NoBlackSky;
 import it.gamerover.nbs.configuration.ConfigManager;
+import it.gamerover.nbs.util.GenericUtil;
 import org.bukkit.World;
 import org.bukkit.WorldType;
 import org.bukkit.entity.Player;
@@ -12,6 +14,7 @@ import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.events.PacketAdapter;
 import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Set;
 
@@ -19,6 +22,12 @@ import java.util.Set;
  * @author gamerover98
  */
 public class NoBlackSkyAdapter extends PacketAdapter {
+
+	/**
+	 * Since 1.16 the packets of PlayLogin and Respawn were edited.
+	 * This string helps to edit correctly these packets.
+	 */
+	private static final String V1_16 = "1.16";
 
 	/**
 	 * This world type will fix the black sky under height 61.
@@ -50,23 +59,48 @@ public class NoBlackSkyAdapter extends PacketAdapter {
 			return;
 		}
 
+		String serverVersion = NoBlackSky.getReflectionContainer()
+				.getMinecraft().getMinecraftServer().getVersion();
+
 		PacketContainer packet = event.getPacket();
 		boolean alwaysEnabled = ConfigManager.isAlwaysEnabled();
 		Set<String> worlds = ConfigManager.getWorlds();
 
 		if (alwaysEnabled || worlds.contains(world.getName())) {
 
-			if (packetType == WrapperPlayServerLogin.TYPE) {
+			GenericUtil.Comparison comparison = GenericUtil.compareServerVersions(serverVersion, V1_16);
 
-				WrapperPlayServerLogin wrapperPlayServerLogin = new WrapperPlayServerLogin(packet);
-				wrapperPlayServerLogin.setLevelType(FLAT_WORLD_TYPE);
-
-			} else if (packetType == WrapperPlayServerRespawn.TYPE) {
-
-				WrapperPlayServerRespawn wrapperPlayServerRespawn = new WrapperPlayServerRespawn(packet);
-				wrapperPlayServerRespawn.setLevelType(FLAT_WORLD_TYPE);
-
+			if (comparison == GenericUtil.Comparison.MINOR) {
+				beforeNetherUpdate(packetType, packet);
+			} else {
+				afterNetherUpdate(packetType, packet);
 			}
+
+		}
+
+	}
+
+	private void afterNetherUpdate(@NotNull PacketType packetType, @NotNull PacketContainer packet) {
+
+		getPlugin().getLogger().info("after");
+		//TODO implementation
+
+	}
+
+	private void beforeNetherUpdate(@NotNull PacketType packetType, @NotNull PacketContainer packet) {
+
+		getPlugin().getLogger().info("before");
+		//TODO remove the above test print
+
+		if (packetType == WrapperPlayServerLogin.TYPE) {
+
+			WrapperPlayServerLogin wrapperPlayServerLogin = new WrapperPlayServerLogin(packet);
+			wrapperPlayServerLogin.setLevelType(FLAT_WORLD_TYPE);
+
+		} else if (packetType == WrapperPlayServerRespawn.TYPE) {
+
+			WrapperPlayServerRespawn wrapperPlayServerRespawn = new WrapperPlayServerRespawn(packet);
+			wrapperPlayServerRespawn.setLevelType(FLAT_WORLD_TYPE);
 
 		}
 
