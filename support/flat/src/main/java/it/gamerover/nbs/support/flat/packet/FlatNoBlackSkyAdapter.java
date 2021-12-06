171080145e95ed8d8cdbbe59695c304cdbaa5aca
gamerover98 <gamerover98@hotmail.it>
2021-12-06 19:16:38 +0100

    Reimplemented 1.13 - 1.15.x packet editing

--- support/flat/src/main/java/it/gamerover/nbs/support/flat/packet/FlatNoBlackSkyAdapter.java
+++ support/flat/src/main/java/it/gamerover/nbs/support/flat/packet/FlatNoBlackSkyAdapter.java
@@ -1,14 +1,20 @@
 package it.gamerover.nbs.support.flat.packet;
 
+import com.comphenix.packetwrapper.WrapperPlayServerLogin;
+import com.comphenix.packetwrapper.WrapperPlayServerRespawn;
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.reflect.StructureModifier;
 import it.gamerover.nbs.core.packet.NoBlackSkyAdapter;
 import it.gamerover.nbs.reflection.ServerVersion;
+import it.gamerover.nbs.reflection.util.Comparison;
+import it.gamerover.nbs.reflection.util.ReflectionUtil;
 import org.bukkit.World;
+import org.bukkit.WorldType;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 
+@SuppressWarnings("squid:S100") // Rename methods name to match the regular expression '^[a-z][a-zA-Z0-9]*$'.
 public class FlatNoBlackSkyAdapter extends NoBlackSkyAdapter {
 
     /**
@@ -21,6 +27,12 @@ public class FlatNoBlackSkyAdapter extends NoBlackSkyAdapter {
      * isFlat field on the 1.16+ packet is at index 4.
      */
     private static final int RESPAWN_IS_FLAT_BOOLEAN_FIELD_INDEX = 1;
+
+    /**
+     * This world type will fix the black sky under height 61.
+     * Wiki at: https://wiki.vg/Protocol#Join_Game
+     */
+    private static final WorldType FLAT_WORLD_TYPE = WorldType.FLAT;
 
     public FlatNoBlackSkyAdapter(@NotNull Plugin plugin, @NotNull ServerVersion currentVersion) {
         super(plugin, currentVersion);
@@ -37,12 +49,61 @@ public class FlatNoBlackSkyAdapter extends NoBlackSkyAdapter {
     @Override
     protected void editPacket(@NotNull PacketContainer packet, @NotNull PacketType packetType) {
 
+        // running server version >= 1.16
+        if (isAtLeastNetherUpdate()) {
+            editPacketAtLeastNetherUpdate(packet, packetType);
+        } else {
+            editPacketBeforeNetherUpdate(packet, packetType);
+        }
+
+    }
+
+    /**
+     * @return True if the running server is at least 1.16.
+     */
+    private boolean isAtLeastNetherUpdate() {
+
+        Comparison result = ReflectionUtil.compareServerVersions(currentVersion, ServerVersion.V1_16);
+        return result == Comparison.SAME || result == Comparison.MAJOR;
+
+    }
+
+    /**
+     * Edit the login or respawn packet if the server version is at least 1.16
+     *
+     * @param packet     The not-null instance of the packet container.
+     * @param packetType The not-null instance of the packet type.
+     */
+    private void editPacketAtLeastNetherUpdate(@NotNull PacketContainer packet, @NotNull PacketType packetType) {
+
         StructureModifier<Boolean> booleans = packet.getBooleans();
 
         if (packetType == PacketType.Play.Server.LOGIN) {
             booleans.write(LOGIN_IS_FLAT_BOOLEAN_FIELD_INDEX, true);
         } else if (packetType == PacketType.Play.Server.RESPAWN) {
             booleans.write(RESPAWN_IS_FLAT_BOOLEAN_FIELD_INDEX, true);
+        }
+
+    }
+
+    /**
+     * Edit the login or respawn packet if the server version is before 1.16.
+     *
+     * @param packet     The not-null instance of the packet container.
+     * @param packetType The not-null instance of the packet type.
+     */
+    private void editPacketBeforeNetherUpdate(@NotNull PacketContainer packet, @NotNull PacketType packetType) {
+
+        if (packetType == WrapperPlayServerLogin.TYPE) {
+
+            WrapperPlayServerLogin wrapperPlayServerLogin = new WrapperPlayServerLogin(packet);
+            wrapperPlayServerLogin.setLevelType(FLAT_WORLD_TYPE);
+
+        } else if (packetType == WrapperPlayServerRespawn.TYPE) {
+
+            WrapperPlayServerRespawn wrapperPlayServerRespawn = new WrapperPlayServerRespawn(packet);
+            wrapperPlayServerRespawn.setLevelType(FLAT_WORLD_TYPE);
+
         }
 
     }
